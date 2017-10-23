d13e6686a7999d911c1883ac7aa64d6252b56276
MoJo <msjche@gmail.com>
2017-10-23 13:22:22 -0700

    update

--- Scripts/laptop_backup_local.sh
+++ Scripts/laptop_backup_local.sh
@@ -4,7 +4,7 @@
 START=$(date +%s)
 #rsync -avz -e --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found/*","/home/msjche/Downloads/*","/home/msjche/Dropbox/*","/home/msjche/Google\ Drive/","/home/msjche/MEGA"} ssh msjche@192.168.1.7:/media/Backup/Laptop /*
 
-rsync -aAXv --delete --exclude={'/dev/*','/proc/*','/sys/*','/tmp/*','/run/*','/mnt/*','/media/*','/home/*','/lost+found'} /* /home/msjche/Backup
+rsync -aAXv --delete --exclude={'/dev/*','/proc/*','/sys/*','/tmp/*','/run/*','/mnt/*','/media/*','/home/*','/lost+found'} /* /run/media/msjche/1921659a-2451-4971-9bd4-e043c7659daf/Laptop/
 
 
 FINISH=$(date +%s)
