3fc4733f49acd9abd6859fc236ddfbe68c86baeb
MoJo <msjche@gmail.com>
2017-11-15 11:04:26 -0800

    update

--- Scripts/Theming/1080.sh
+++ Scripts/Theming/1080.sh
@@ -2,5 +2,6 @@
 
 xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode eDP1 1920x1080_60.00
-xrandr --output eDP1 --mode 1920x1080_60.00
+#xrandr --output eDP1 --mode 1920x1080_60.00
+xrandr --output eDP1 --mode 1920x1080
 xrdb -merge ~/.Xresources
--- Scripts/Theming/1440.sh
+++ Scripts/Theming/1440.sh
@@ -5,3 +5,15 @@ xrandr --addmode eDP1 2560x1440_60.00
 xrandr --output eDP1 --mode 2560x1440_60.00
 
 xrdb -merge ~/.Xresources
+
+#cp ~/.config/awesome/PROFILES/msjche/rc.lua ~/.config/Awesome/PROFILES/msjche/rc.lua.1080
+#cp ~/.config/awesome/PROFILES/msjche/rc.lua.1440 ~/.config/Awesome/PROFILES/msjche/rc.lua
+#cp ~/.config/awesome/PROFILES/msjche/themes/msjche/theme.lua ~/.config/Awesome/PROFILES/msjche/themes/msjche/theme.lua.1080
+#cp ~/.config/awesome/PROFILES/msjche/themes/msjche/theme.lua.1440 ~/.config/Awesome/PROFILES/msjche/themes/msjche/theme.lua
+#
+#
+#echo 'awesome.restart()' | awesome-client
+#
+#sleep 3
+#
+#notify-send "1440p resolution set"
